c2aafdffb0c9bbb918f0b6edaea93996662275e2
Andreas Traa Utkilen <andreastraautkilen@gmail.com>
2020-12-10 09:42:22 +0100

    error in webpack fix

--- webpack.config.js
+++ webpack.config.js
@@ -4,54 +4,56 @@ const VueLoaderPlugin = require("vue-loader/lib/plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    mode: "production",
-    entry: {
-      stepperVue: "./src/stepper.es6",
-      diagramVue: "./src/diagram.es6",
-      // storyVue: "./src/story.es6",
-      promocardHorizontalVue: "./src/promocard-horizontal.es6",
-      mediaPlayerVue: "./src/media-player.es6"
+  mode: "production",
+  entry: {
+    stepperVue: "./src/stepper.es6",
+    diagramVue: "./src/diagram.es6",
+    // storyVue: "./src/story.es6",
+    promocardHorizontalVue: "./src/promocard-horizontal.es6",
+    mediaPlayerVue: "./src/media-player.es6"
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    // Bad workaround for treeshaking vuetify css
+    new MiniCssExtractPlugin({
+      publicPath: "/_/asset/forsvno:[hash]/",
+      path: require("path").resolve(__dirname, "/dist/css/"),
+      filename: "style.css",
+      chunkFilename: "styleChunk.css"
+    })
+  ],
+  module: {
+    rules: [{
+      test: /\.es6?$/,
+      use: "babel-loader",
+      exclude: /node_modules/
     },
-    plugins: [
-      new VueLoaderPlugin(),
-      // Bad workaround for treeshaking vuetify css
-      new MiniCssExtractPlugin({
-        publicPath: "/_/asset/forsvno:[hash]/",
-        path: require("path").resolve(__dirname, "/dist/css/"),
-        filename: "style.css",
-        chunkFilename: "styleChunk.css"
-      })
-    ],
-    module: {
-      rules: [{
-          test: /\.es6?$/,
-          use: "babel-loader",
-          exclude: /node_modules/
-        },
-        {
-          test: /\.vue$/,
-          use: [
-            "vue-loader"
-          ]
-        },
-        {
-          test: /\.s[ac]ss$/i,
-          use: [
-            MiniCssExtractPlugin.loader,
-            "css-loader",
-            "sass-loader"
-          ]
-        },
-        resolve: {
-          alias: {
-            vue$: "vue/dist/vue.esm.js"
-          },
-          extensions: [".es6", ".js", ".vue", ".ts"]
-        },
-        output: {
-          publicPath: "/_/asset/forsvno:[hash]/",
-          path: require("path").resolve(__dirname, "../forsvno/src/main/resources/assets"),
-          filename: "js/[name].js",
-          chunkFilename: "js/[name].chunk.js"
-        }
-      };
+    {
+      test: /\.vue$/,
+      use: [
+        "vue-loader"
+      ]
+    },
+    {
+      test: /\.s[ac]ss$/i,
+      use: [
+        MiniCssExtractPlugin.loader,
+        "css-loader",
+        "sass-loader"
+      ]
+    }
+    ]
+  },
+  resolve: {
+    alias: {
+      vue$: "vue/dist/vue.esm.js"
+    },
+    extensions: [".es6", ".js", ".vue", ".ts"]
+  },
+  output: {
+    publicPath: "/_/asset/forsvno:[hash]/",
+    path: require("path").resolve(__dirname, "../forsvno/src/main/resources/assets"),
+    filename: "js/[name].js",
+    chunkFilename: "js/[name].chunk.js"
+  }
+};
